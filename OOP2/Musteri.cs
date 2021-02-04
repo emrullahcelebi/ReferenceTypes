0913116811f05fefeddabcbf92df6045f701e5da
Emrah <cumali20182019@gmail.com>
2021-02-04 16:33:19 +0200

    odev4

---
+++ OOP2/Musteri.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    class Musteri
+    {
+
+        public int Id { get; set; }
+        public string MusteriNo { get; set; }
+
+        //neden int değil de string cünkü veri ile işlem yapmıyorsun 
+        //oyuzden metinsel tanımlamayı tercih etmek lazım
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            TasitKrediManager tasitKrediManager = new TasitKrediManager();
+            KonutKrediManager konutKrediManager = new KonutKrediManager();
+
+
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            //basvuruManager.BasvuruYap(konutKrediManager);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() { };
+
+            basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+            
+        }
+    }
+}
