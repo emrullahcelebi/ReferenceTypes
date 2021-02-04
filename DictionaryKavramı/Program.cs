adf1bdc6318b0aeeb9c6e9d0b40e0de878022b47
Emrah <cumali20182019@gmail.com>
2021-02-04 13:47:21 +0200

    odev 4

---
+++ DictionaryKavramı/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryKavramı
+{
+    class Program
+    {
+        //key e uygun olarak değer saklar
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> okulNumarasıAdı = new Dictionary<string, int>();
+            okulNumarasıAdı.Add("Ali",123);
+            okulNumarasıAdı.Add("Emrah", 456);
+            okulNumarasıAdı.Add("Cemil", 789);
+            okulNumarasıAdı.Add("Tahir", 102);
+            okulNumarasıAdı.Add("Adnan", 216);
+
+            foreach (var veriler in okulNumarasıAdı)
+            {
+                Console.WriteLine(veriler);
+
+            }
+
+        }
+    }
+    
+
+}
---
+++ Generics2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Generics2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> sehirler = new List<string>();
+            sehirler.Add("Ankara");
+            MyList<string> sehirler2 = new MyList<string>();
+            sehirler2.Add("Ankara");
+        }
+    }
+
+    class MyList<T>
+    {
+        public void Add(string item)
+        {
+
+        }
+    }
+}
